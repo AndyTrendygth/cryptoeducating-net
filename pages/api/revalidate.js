41f3519cd909c92be12ca2f84330c19590836b69
AndyTrendygth <andreaskrenn@outlook.com>
2022-05-15 13:33:20 +0200

    commit25

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     // Check for secret to confirm this is a valid request
-    if (req.body.secret !== process.env.MY_SECRET_TOKEN) {
+    if (req.headers['Secret'] !== process.env.MY_SECRET_TOKEN) {
       return res.status(401).json({ message: 'Invalid token' })
     }
   
