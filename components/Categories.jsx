af4b47a324f865ef8b368be94957a10684cf7810
AndyTrendygth <andreaskrenn@outlook.com>
2022-07-10 16:15:37 +0200

    commit48

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,29 +1,43 @@
 import Link from 'next/link'
 import React from 'react'
-import styles from './Header.module.css'
+import styles from './Categories.module.scss'
 
 const Categories = () => {
-  return (
-      <div className='flex flex-col items-center mt-5'>
+return (
+  <div className='flex flex-col items-center mt-5'>
     <h1 className={` ${styles.h1} text-4xl font-black p-2`}>OUR CATEGORIES</h1>
-    <div className='grid-cols-3 m-3'>
-        <div>
-        <Link href="/category/defi"><a className='text-3xl font-bold m-3 hover:text-purple-700'>DeFi</a></Link>
-        <Link href="/category/exchanges"><a className='text-3xl font-bold m-3 hover:text-purple-700'>Exchanges</a></Link>
-        <Link href="/category/blockchain"><a className='text-3xl font-bold m-3 hover:text-purple-700'>Blockchain</a></Link>
+    <div className='flex flex-col gap-14 md:flex md:flex-row'>    
+      <div className={`${styles.circle} `}>
+        <div className={`${styles.text}`}>
+          <p className='text-center font-bold text-xl mr-16 md:mr-14 sm:text-2xl md:text-4xl'>BLOCKCHAIN</p>
+          <div className='text-md font-normal mt-1 mr-3 sm:text-xl md:text-xl lg:text-2xl'>FROM LAYER-1 TO PROTOCOL</div>
+          <p className='text-center font-bold text-xl sm:text-2xl md:text-4xl'>WALLETS</p>
+          <div className='text-lg font-normal mt-1 mr-3 md:text-xl lg:text-2xl'>BROWSER TO HARDWARE</div>
         </div>
-        <Link href="/category/nft"><a className='text-3xl font-bold m-3 hover:text-purple-700'>NFT</a></Link>
-        <Link href="/category/wallets"><a className='text-3xl font-bold m-3 hover:text-purple-700'>Wallets</a></Link>
-        <Link href="/category/bitcoin"><a className='text-3xl font-bold m-3 hover:text-purple-700'>Bitcoin</a></Link>
-        <div>
-        <Link href="/category/technical-analysis"><a className='text-3xl font-bold m-3 hover:text-purple-700'>TA</a></Link>
-        <Link href="/category/metaverse"><a className='text-3xl font-bold m-3 hover:text-purple-700'>Metaverse</a></Link>
-        <Link href="/category/altcoins"><a className='text-3xl font-bold m-3 hover:text-purple-700'>Altcoins</a></Link>
+      </div>
+      
+      <div className='flex flex-col'>
+        <div className={`${styles.ellips_1}`}> 
+          <div className='mt-8 flex flex-col'>
+            <p className='text-xl font-bold sm:text-2xl md:text-4xl'>BITCOIN</p>
+            <div className='text-md font-normal mt-1 mr-3 md:text-xl lg:text-2xl'>EVERYTHING ABOUT OUR FAVORITE COIN</div>
+          </div>
+          <div className='mt-2'>
+          <p className='text-lg font-bold sm:text-2xl md:text-4xl'>NFT</p>
+            <div className='text-md font-normal sm:text-xl md:text-xl lg:text-2xl'>MORE THAN RANDOM JPEGS</div>
+          </div>
         </div>
-        <Link href="/category/deep-dives"><a className='text-3xl font-bold m-3 hover:text-purple-700'>Deep Dives</a></Link>
-        <Link href="/category/guides"><a className='text-3xl font-bold m-3 hover:text-purple-700'>Guides</a></Link>
-        <Link href="/category/crypto-basics"><a className='text-3xl font-bold m-3 hover:text-purple-700'>Crypto Basics</a></Link>
-    </div>
+      <div className={`${styles.ellips_2}`}>
+        <div className={`mt-6`}>
+      <p className=' font-bold text-xl sm:text-2xl md:text-4xl'>DEFI</p>
+          <div className='text-center text-md font-normal mt-1 sm:text-xl md:text-xl lg:text-2xl'>STAKING AND MORE</div>
+          <p className=' font-bold text-xl mt-6 sm:text-md md:text-4xl'>ALTCOINS</p>
+          <div className='text-md font-normal mt-1 sm:text-xl md:text-xl lg:text-2xl'>FOR THE DEGENS</div>
+          </div>
+      </div>
+   </div>
+  </div>
+  <div className='font-bold text-xl mt-10 sm:text-2xl md:text-3xl lg:text-4xl'>AND MORE!</div>
     </div>
   )
 }
